5c6510e2df9a0ff65f8f7d12e2ea0ce441c02e18
mrazinshaikh <razinshakh8732@gmail.com>
2024-04-21 13:24:30 +0530

    timestempsD bluprint macro added

--- DB-Config.md
+++ DB-Config.md
@@ -7,3 +7,28 @@
 - name
 - email
 - phone_number - username
+
+
+# Chats
+
+- id (Primary Key)
+- created_at
+- updated_at
+
+# Messages
+
+- id (Primary Key)
+- chat_id (Foreign Key from Chats)
+- user_id (Foreign Key from Users, indicating who sent the message)
+- message
+- status (e.g., sent, delivered, seen)
+- created_at
+- updated_at
+
+# 
+- id (Primary Key)
+- chat_id (Foreign Key from Chats)
+- user_id (Foreign Key from Users)
+- last_read_at (Timestamp of the last message read by the user)
+- created_at
+- updated_at
--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,42 +10,43 @@
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  */
 
-
 namespace App\Models{
-/**
- * 
- *
- * @property int $id
- * @property string $name
- * @property string|null $email
- * @property string|null $phone_number
- * @property \Illuminate\Support\Carbon|null $email_verified_at
- * @property mixed $password
- * @property string|null $two_factor_secret
- * @property string|null $two_factor_recovery_codes
- * @property string|null $two_factor_confirmed_at
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
- * @property-read int|null $notifications_count
- * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneNumber($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorConfirmedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- */
-	class User extends \Eloquent {}
+    /**
+     * @property int $id
+     * @property string $name
+     * @property string|null $email
+     * @property string|null $phone_number
+     * @property \Illuminate\Support\Carbon|null $email_verified_at
+     * @property mixed $password
+     * @property string|null $two_factor_secret
+     * @property string|null $two_factor_recovery_codes
+     * @property string|null $two_factor_confirmed_at
+     * @property string|null $remember_token
+     * @property \Illuminate\Support\Carbon|null $created_at
+     * @property \Illuminate\Support\Carbon|null $updated_at
+     * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+     * @property-read int|null $notifications_count
+     *
+     * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+     * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|User query()
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneNumber($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorConfirmedAt($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+     *
+     * @mixin \Eloquent
+     */
+    class User extends \Eloquent
+    {
+    }
 }
-
